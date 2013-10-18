45a379dd38a394194ccaacd110cff1d40d4f6cab
Leonardo Shinagawa <leonardo.shinagawa@hi-midia.com>
2013-10-18 11:50:22 -0300

    some fixes

--- test/2_everything_else/long_access_token.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$token = 'a:2:{s:12:"access_token";s:196:"CAAIZB0CHumqcBAByyba52mXR8B2Xv1hh1m3or2WAnxZBQsvN8slyl6nbhgoqzNttHDajYloHP2pA4EkqPOwelYaMgvgv1cnUiZCjOFpjDT04KQb0EYugp0hUXWzm77RX6Kw2zqRA8ShfZC8kWCeH54ZAWb0tKlzLgVVSTE3ZBLi0XhLEI3YasgnQcwGeYxS78ZD";s:7:"expires";i:5000;}';
-
-return unserialize($token);
